a252fec65886cede2e2c2d21780c682d59b881e4
gwthanhtuanvp88 <tuanntgt00538@fpt.edu.vn>
2019-01-04 23:15:34 +0000

    save and load model

--- NeurualNetwork/ConvolutionNeuralNetwork/shallownet_cifar10.py
+++ NeurualNetwork/ConvolutionNeuralNetwork/shallownet_cifar10.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from keras.optimizers import SGD
 from keras.datasets import cifar10
+from keras.models import load_model
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,6 +28,11 @@ model = ShallowNet.build(32, 32, 3, 10)
 sgd = SGD()
 model.compile(sgd, 'categorical_crossentropy', ['accuracy'])
 H = model.fit(X_train, y_train, 64, 30, validation_data=(X_test, y_test))
+
+#save and load model
+model.save('./model/shallow_weight.hdf5')
+model = load_model('./model/shallow_weight.hdf5')
+# model.predict....
 
 # graph of loss and accuracy
 fig = plt.figure()
