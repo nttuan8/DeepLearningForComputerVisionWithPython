b92b4ad3ec49b79bc9c491cda9853c7c5751f080
gwthanhtuanvp88 <tuanntgt00538@fpt.edu.vn>
2019-01-05 14:47:04 +0000

    Learning rate decay

--- NeurualNetwork/ConvolutionNeuralNetwork/miniVGGNet_cifar10_weightDecay.py
+++ NeurualNetwork/ConvolutionNeuralNetwork/miniVGGNet_cifar10_weightDecay.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jan  5 10:48:37 2019
+Created on Sat Jan  5 14:03:40 2019
 
 @author: DELL
 """
@@ -8,12 +8,21 @@ Created on Sat Jan  5 10:48:37 2019
 from miniVGGNet import MiniVGGNet
 
 from keras.datasets import cifar10
+from keras.callbacks import LearningRateScheduler
 from keras.optimizers import SGD
 
 from sklearn.preprocessing import LabelBinarizer
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+def stepDecay(epoch):
+    initAlpha = 0.01
+    factor = 0.25
+    dropEvery = 5
+    
+    alpha = initAlpha * (factor ** np.floor((1+epoch)/dropEvery))
+    return float(alpha)
 
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 X_train = X_train.astype('float')/255
@@ -26,8 +35,9 @@ y_test = lb.fit_transform(y_test)
 numOfEpoch = 40
 
 model = MiniVGGNet.build(32, 32, 3, 10)
+callbacks = [LearningRateScheduler(stepDecay)]
 model.compile(SGD(lr=0.01, momentum=0.9, nesterov=True), 'categorical_crossentropy', ['accuracy'])
-H = model.fit(X_train, y_train, 128, numOfEpoch, validation_data=(X_test, y_test))
+H = model.fit(X_train, y_train, 128, numOfEpoch, validation_data=(X_test, y_test), callbacks=callbacks)
 
 # graph of loss and accuracy
 fig = plt.figure()
